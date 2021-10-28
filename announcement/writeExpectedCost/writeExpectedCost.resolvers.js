bffa8723d575deb90ffd86a4f62d354e064eb6b9
walkerJung <jjh_walker@kakao.com>
2021-10-28 17:24:21 +0900

    2021-10-28 17:24

---
+++ announcement/writeExpectedCost/writeExpectedCost.resolvers.js
@@ -0,0 +1,26 @@
+import client from "../../client";
+
+export default {
+  Mutation: {
+    writeExpectedCost: async (_, { announcementCode, expectedCost }) => {
+      try {
+        await client.announcementCode.update({
+          where: {
+            code: announcementCode,
+          },
+          data: {
+            expectedCost,
+          },
+        });
+        return {
+          ok: true,
+        };
+      } catch (e) {
+        return {
+          ok: false,
+          error: "예상간병비 등록이 실패하였습니다. 다시 시도해주세요.",
+        };
+      }
+    },
+  },
+};
