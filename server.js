a660f0e4e46116fd696709469aedcc7e9c577d0e
walkerJung <jjh_walker@kakao.com>
2021-10-29 10:19:37 +0900

    2021-10-29 10:19

--- server.js
+++ server.js
@@ -2,11 +2,26 @@ require("dotenv").config();
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema.js";
+import { getUser } from "./application/common/utils";
 
 const PORT = process.env.PORT;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async (ctx) => {
+    if (ctx.req) {
+      return {
+        loggedInUser: await getUser(ctx.req.headers.token),
+      };
+    } else {
+      const {
+        connection: { context },
+      } = ctx;
+      return {
+        loggedInUser: context.loggedInUser,
+      };
+    }
+  },
 });
 
 const app = express();
