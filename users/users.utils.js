3a9db0d2a691b8397b9d1d08a7557eda0951e0e0
walkerJung <jjh_walker@kakao.com>
2021-10-29 11:01:54 +0900

    2021-10-29 11:01

--- users/users.utils.js
+++ users/users.utils.js
@@ -3,11 +3,12 @@ import client from "../client";
 
 export const getUser = async (token) => {
   try {
-    console.log("token:" + token);
     if (!token) {
       return null;
     }
-    const userId = await jwt.verify(token, process.env.SECRET_KEY);
+    console.log(process.env.SECRET_KEY);
+    console.log("qweweqwe");
+    const { userId } = await jwt.verify(token, process.env.SECRET_KEY);
     console.log(userId);
     const user = await client.user.findFirst({ where: { code } });
     if (user) {
