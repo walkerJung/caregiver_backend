33e658e9a5cc7185bca18d37e1d3ee1686505838
walkerJung <92363576+walkerJung@users.noreply.github.com>
2021-11-09 16:08:10 +0900

    2021-11-09 16:08

---
+++ users/users.typeDefs.js
@@ -0,0 +1,13 @@
+import { gql } from "apollo-server";
+
+export default gql`
+  type User {
+    code: Int!
+    userId: String!
+    userType: String!
+    userName: String!
+    password: String!
+    sex: String!
+    phone: String!
+  }
+`;
