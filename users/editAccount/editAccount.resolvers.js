4fbad86b02929de329e701fbbb96697123890cee
walkerJung <92363576+walkerJung@users.noreply.github.com>
2021-11-09 16:27:50 +0900

    2021-11-09 16:27

--- users/editAccount/editAccount.resolvers.js
+++ users/editAccount/editAccount.resolvers.js
@@ -5,6 +5,7 @@ export default {
   Mutation: {
     editAccount: async (_, { phone, userName, password }, { loggedInUser }) => {
       try {
+        const uglyPassword = await bcrypt.hash(password, 10);
         await client.user.update({
           where: {
             code: loggedInUser.code,
@@ -12,7 +13,7 @@ export default {
           data: {
             phone,
             userName,
-            password,
+            password: uglyPassword,
           },
         });
         return {
