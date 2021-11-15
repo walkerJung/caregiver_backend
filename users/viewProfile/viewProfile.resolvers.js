dce6659ad9bb603647d7d3b2f4a9831e9abe6989
walkerJung <92363576+walkerJung@users.noreply.github.com>
2021-11-15 18:01:17 +0900

    2021-11-15 18:01

--- users/viewProfile/viewProfile.resolvers.js
+++ users/viewProfile/viewProfile.resolvers.js
@@ -2,14 +2,20 @@ import client from "../../client";
 
 export default {
   Query: {
-    viewProfile: (_, { code }) =>
-      client.user.findMany({
-        where: {
-          code,
-        },
-        include: {
-          CaregiverInfo: true,
-        },
-      }),
+    viewProfile: (_, { code }) => {
+      try {
+        const user = client.user.findUnique({
+          where: {
+            code,
+          },
+          include: {
+            CaregiverInfo: true,
+          },
+        });
+        return user;
+      } catch (e) {
+        return false;
+      }
+    },
   },
 };
