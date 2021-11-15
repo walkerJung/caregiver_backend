6d3421c1545848415ede880b969cd5943110c03f
walkerJung <92363576+walkerJung@users.noreply.github.com>
2021-11-15 18:28:35 +0900

    2021-11-15 18:28

--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -1,6 +1,23 @@
 import { gql } from "apollo-server";
 
 export default gql`
+  type CaregiverInfo {
+    code: Int!
+    userCode: Int!
+    address: String!
+    addressDetail: String!
+    residentNumber: String!
+    idCard: String!
+    bankInfo: String!
+    smoke: String!
+    drink: String!
+    mealCare: String!
+    urineCare: String!
+    suctionCare: String!
+    moveCare: String!
+    bedCare: String!
+  }
+
   type User {
     code: Int!
     userId: String!
