a5265b76290f1a3be50583802748a6c299d4068d
walkerJung <92363576+walkerJung@users.noreply.github.com>
2021-11-19 11:13:39 +0900

    2021-11-19 11:13

--- notice/listNotice/listNotice.resolvers.js
+++ notice/listNotice/listNotice.resolvers.js
@@ -5,6 +5,7 @@ export default {
     listNotice: async (_) => {
       try {
         const notices = await client.notice.findMany();
+        console.log(notices);
         return {
           notices,
           result: true,
