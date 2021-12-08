d75846ba661419fdbb7675320d2d5ba71b9b00cf
walkerJung <92363576+walkerJung@users.noreply.github.com>
2021-12-08 14:37:38 +0900

    2021-12-08 14:37

--- users/editAccount/editAccount.resolvers.js
+++ users/editAccount/editAccount.resolvers.js
@@ -19,21 +19,50 @@ export default {
           throw new Error("잘못된 접근입니다.");
         }
 
-        const uglyPassword = await bcrypt.hash(password, 10);
-        await client.user.update({
-          where: {
-            code: user.code,
-          },
-          data: {
-            phone,
-            userName,
-            password: uglyPassword,
-          },
-        });
-        return {
-          ok: true,
-        };
+        if (phone) {
+          await client.user.update({
+            where: {
+              code: user.code,
+            },
+            data: {
+              phone,
+            },
+          });
+          return {
+            ok: true,
+          };
+        }
+
+        if (userName) {
+          await client.user.update({
+            where: {
+              code: user.code,
+            },
+            data: {
+              userName,
+            },
+          });
+          return {
+            ok: true,
+          };
+        }
+
+        if (password) {
+          const uglyPassword = await bcrypt.hash(password, 10);
+          await client.user.update({
+            where: {
+              code: user.code,
+            },
+            data: {
+              password: uglyPassword,
+            },
+          });
+          return {
+            ok: true,
+          };
+        }
       } catch (e) {
+        console.log(e);
         return {
           ok: false,
           error: "회원정보 수정을 실패하였습니다. 관리자에게 문의해주세요.",
