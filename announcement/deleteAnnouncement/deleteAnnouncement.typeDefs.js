347187ddad6367ccf8a0c43b193bcb5ec75fa111
walkerJung <92363576+walkerJung@users.noreply.github.com>
2021-12-13 12:58:04 +0900

    2021-12-13 12:58

--- announcement/deleteAnnouncement/deleteAnnouncement.typeDefs.js
+++ announcement/deleteAnnouncement/deleteAnnouncement.typeDefs.js
@@ -2,9 +2,6 @@ import { gql } from "apollo-server";
 
 export default gql`
   type Mutation {
-    deleteAnnouncement(
-      userCode: Int!
-      announcementCode: Int!
-    ): MutationResponse!
+    deleteAnnouncement(announcementCode: Int!): MutationResponse!
   }
 `;
