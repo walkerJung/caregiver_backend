ac9a306035c413c9d7e0026836c8cc450f0c4135
walkerJung <92363576+walkerJung@users.noreply.github.com>
2021-12-15 12:45:42 +0900

    2021-12-15 12:45

--- announcement/listAnnouncementApplication/listAnnouncementApplication.resolvers.js
+++ announcement/listAnnouncementApplication/listAnnouncementApplication.resolvers.js
@@ -7,6 +7,11 @@ export default {
         where: { code: announcementCode },
       });
     },
+    user: ({ userCode }) => {
+      return client.user.findUnique({
+        where: { code: userCode },
+      });
+    },
   },
   Query: {
     listAnnouncementApplication: async (
